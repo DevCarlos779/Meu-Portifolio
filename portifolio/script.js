7e9192c156c72728599bfe4a4169bc53ae9dd6c3
Carlos <devcarlos0112@gmail.com>
2025-11-12 16:19:36 -0300

    Adicionando projeto Focus Flow

--- portifolio/script.js
+++ portifolio/script.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById("mobile-menu");
 const navbar = document.getElementById("nav-bar");
 const nav_links = document.getElementsByClassName("item-nav");
+const focusFLow = document.getElementById("focus-flow");
 
 menu.addEventListener("click", () => {
     navbar.classList.toggle("active")
@@ -11,6 +12,10 @@ Array.from(nav_links).forEach(link => {
         navbar.classList.remove("active"); 
     });
 });
+
+focusFLow.addEventListener("click", () => {
+    window.open('https://focus-flow-mocha.vercel.app', '_blank');
+})
 
 
 
